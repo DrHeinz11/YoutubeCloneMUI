19536677b95eabe68deb0e662cf9d0dc4463870d
DrHeinz11 <franco.martin.1998m@gmail.com>
2022-08-26 13:28:49 -0300

    [Upload] Create Components Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        Navbar
+        <Routes>
+          <Route path="/" exact elements={<Feed />} />
+          <Route path="/video/:id" elements={<VideoDetails />} />
+          <Route path="/channel/:id" elements={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" elements={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 };
 
 export default App;

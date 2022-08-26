d1b8f627d220f26c69fad39d061a9c7f6323cd40
DrHeinz11 <franco.martin.1998m@gmail.com>
2022-08-26 15:10:03 -0300

    [Update] Component (SideBar,Feed)

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,29 @@
+import { Stack } from "@mui/material";
 import React from "react";
+import { categories } from "../utils/constants";
 
-const SideBar = () => {
-  return <div>SideBar</div>;
-};
-
+const SideBar = () => (
+  <Stack
+    direction="row"
+    sx={{
+      overflowY: "auto",
+      height: { sx: "auto", md: "95%" },
+      flexDirection: { md: "column" },
+    }}
+  >
+    {categories.map((category) => (
+      <button
+        className="category-btn"
+        key={category.name}
+        style={{
+          background: category.name === "New" && "#fc1503",
+          color: "white",
+        }}
+      >
+        <span>{category.icon}</span>
+        <span>{category.name}</span>
+      </button>
+    ))}
+  </Stack>
+);
 export default SideBar;
